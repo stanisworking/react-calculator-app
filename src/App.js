e29df09afe51412b22f944b04a4ea12f0c3b3588
Stanley Haryoto <stanley.haryoto@gmail.com>
2020-05-23 07:35:29 +0800

    Initial app setup for calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>This is a react calculator app</h3>
     </div>
   );
 }
 
-export default App;
+export default app;
