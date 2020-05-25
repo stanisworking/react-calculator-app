d31db0974be9d597c28f084539745d8219ba5d0f
Stanley Haryoto <stanley.haryoto@gmail.com>
2020-05-25 08:04:10 +0800

    Implemented calculator functionality

--- src/layout/Calculator/Keypad/Keypad.js
+++ src/layout/Calculator/Keypad/Keypad.js
@@ -3,43 +3,37 @@ import KeypadRow from './KeypadRow/KeypadRow';
 import Button from '../../../components/Button/Button';
 import LargeButton from '../../../components/Button/LargeButton/LargeButton';
 
-const keypad = () => {
+const keypad = (props) => {
 	return (
 		<section className='keypad'>
 			<KeypadRow>
-				<Button>C</Button>
-				<Button>&larr;</Button>
-				<Button>%</Button>
-				<Button>/</Button>
+				<Button onButtonPress={props.onButtonPress}>C</Button>
+				<Button onButtonPress={props.onButtonPress}>&larr;</Button>
+				<Button onButtonPress={props.onButtonPress}>%</Button>
+				<Button onButtonPress={props.onButtonPress}>/</Button>
 			</KeypadRow>
 			<KeypadRow>
-				<Button>9</Button>
-				<Button>8</Button>
-				<Button>7</Button>
-				<Button>
-					<i class='large material-icons'>close</i>
-				</Button>
+				<Button onButtonPress={props.onButtonPress}>9</Button>
+				<Button onButtonPress={props.onButtonPress}>8</Button>
+				<Button onButtonPress={props.onButtonPress}>7</Button>
+				<Button onButtonPress={props.onButtonPress}>x</Button>
 			</KeypadRow>
 			<KeypadRow>
-				<Button>6</Button>
-				<Button>5</Button>
-				<Button>4</Button>
-				<Button>
-					<i class='material-icons'>remove</i>
-				</Button>
+				<Button onButtonPress={props.onButtonPress}>6</Button>
+				<Button onButtonPress={props.onButtonPress}>5</Button>
+				<Button onButtonPress={props.onButtonPress}>4</Button>
+				<Button onButtonPress={props.onButtonPress}>-</Button>
 			</KeypadRow>
 			<KeypadRow>
-				<Button>3</Button>
-				<Button>2</Button>
-				<Button>1</Button>
-				<Button>
-					<i class='material-icons'>add</i>
-				</Button>
+				<Button onButtonPress={props.onButtonPress}>3</Button>
+				<Button onButtonPress={props.onButtonPress}>2</Button>
+				<Button onButtonPress={props.onButtonPress}>1</Button>
+				<Button onButtonPress={props.onButtonPress}>+</Button>
 			</KeypadRow>
 			<KeypadRow>
-				<Button>0</Button>
-				<Button>.</Button>
-				<LargeButton>=</LargeButton>
+				<Button onButtonPress={props.onButtonPress}>0</Button>
+				<Button onButtonPress={props.onButtonPress}>.</Button>
+				<LargeButton onButtonPress={props.onButtonPress}>=</LargeButton>
 			</KeypadRow>
 		</section>
 	);
--- src/layout/Calculator/Screen/Screen.js
+++ src/layout/Calculator/Screen/Screen.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ResultScreen from './ResultScreen/ResultScreen';
 import ComputationScreen from './ComputeScreen/ComputeScreen';
 
-const screen = () => {
+const screen = (props) => {
 	return (
 		<section
 			className='right-align red lighten-4'
 			style={{ height: '25vh', padding: '10px' }}
 		>
-			<ResultScreen />
-			<ComputationScreen />
+			<ResultScreen>{props.result}</ResultScreen>
+			<ComputationScreen>{props.equation}</ComputationScreen>
 		</section>
 	);
 };
